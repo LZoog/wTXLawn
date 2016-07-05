9b049cfd6b1ae7b29701bb64e31e54c62bd89781
Lauren Zugai <lauren@zugai.com>
2016-07-04 20:42:16 -0500

    Navbar is fixed on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
 
 $(function(){
+
+  var headHeight = $('#top').height() + $('nav').height() + 28;
+  $('nav').toggleClass('sticky', $(this).scrollTop() >= headHeight);
+
+  $(window).scroll(function(){
+    $('nav').toggleClass('sticky', $(this).scrollTop() >= headHeight);
+  });
+
+//   $('#nav').onePageNav({
+//     currentClass: 'current',
+//     changeHash: true,
+//     scrollSpeed: 750,
+//     scrollThreshold: 0.5,
+//     filter: '',
+//     easing: 'swing',
+//     begin: function() {
+//         //I get fired when the animation is starting
+//     },
+//     end: function() {
+//         //I get fired when the animation is ending
+//     },
+//     scrollChange: function($currentListItem) {
+//         //I get fired when you enter a section and I pass the list item of the section
+//     }
+// });
+
 });
