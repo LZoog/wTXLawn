e27f78536d1e1c58e84dc899dfcfc8eac1e979a3
Lauren Zugai <lauren@zugai.com>
2016-07-06 00:09:05 -0500

    Add navbar JS

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,7 @@ $(function(){
     var top;
     anchors.each(function(){
       if (anchors[3]) {
+        // #contact anchor
         top = $(this).offset().top - 450;
       } else {
         top = $(this).offset().top;
@@ -37,30 +38,6 @@ $(function(){
     sticky();
     active();
   });
-
-  /* ACTIVE STATE NAV */
-  //on scroll
-  //var currentScroll = $(this).scrollTop();
-
-  // navHeight = $('nav')
-  // , nav_height = nav.outerHeight();
-  //
-  // $(window).on('scroll', function () {
-  //   var cur_pos = $(this).scrollTop();
-  //
-  //   sections.each(function() {
-  //     var top = $(this).offset().top - nav_height,
-  //         bottom = top + $(this).outerHeight();
-  //
-  //     if (cur_pos >= top && cur_pos <= bottom) {
-  //       nav.find('a').removeClass('active');
-  //       sections.removeClass('active');
-  //
-  //       $(this).addClass('active');
-  //       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-  //     }
-  //   });
-  // });
 
   /* SMOOTH SCROLLING */
   $(function() {
