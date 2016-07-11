480b359c1434d8c7793df719d0561552234d94f1
Lauren Zugai <lauren@zugai.com>
2016-07-11 00:22:08 -0500

    fixed bug in active navbar state

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 
 $(function(){
 
-  /* STICKY NAV */
+  var a = $(window).scrollTop();
+
+  // set opacity to 0 with JS instead of CSS b/c animations are done with JS
+  $('.benefit').each(function(){$(this).css('opacity', '0');});
+
+  /* variables for active nav & animations */
+  var anchors = $('#top, #about, #grasses, #contact');
+  var topBenefits = $('#why').offset().top + 200;
+  var bottomBenefits = topBenefits - $('#why').outerHeight(true) - 300;
+  // variables for sticky nav
   var headHeight = $('#top').outerHeight(true);
   var navHeight = $('nav').outerHeight(true);
-  function sticky() {
+
+  onScroll();
+
+  function onScroll() {
+    // set/unset sticky nav
     if ($(window).scrollTop() >= headHeight) {
       $('nav').addClass('sticky');
       $('#logo').addClass('fade-in');
@@ -14,57 +27,35 @@ $(function(){
       $('#logo').removeClass('fade-in');
       $('#fence').css('margin-top', '0px');
     }
-  }
-  sticky();
 
-  /* ACTIVE STATE NAV && ANIMATIONS */
-  $('.benefit').each(function(){$(this).css('opacity', '0');});
-  var anchors = $('#top, #about, #grasses, #contact');
-  var topBenefits = $('#why').offset().top + 200;
-  var bottomBenefits = topBenefits - $('#why').outerHeight(true) - 300;
+    // check for active nav
 
-  var top;
-  anchors.each(function(){
-    if (anchors[3]) {
-      // #contact anchor
-      top = $(this).offset().top - 300;
-    } else {
-      top = $(this).offset().top;
-    }
-  });
+    // get top of sections for navbar & animations, set active on navbar
+    var top;
+    anchors.each(function(){
+      if (anchors[3]) {
+        // #contact anchor
+        top = $(this).offset().top - 300;
+      } else {
+        top = $(this).offset().top;
+      }
 
-  function activeAndAnimations() {
+      if ($(window).scrollTop() >= top) {
+        $('nav').find('a').removeClass('active');
+        $('nav').find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      }
+    });
 
     // benefits animation
     if ((topBenefits >= $(window).scrollTop() && $(window).scrollTop() >= bottomBenefits) && !$('.benefit').hasClass('fade-in')) {
       $('.benefit').each(function(i) {
-        $(this).addClass('fade-in benefit-' + parseInt(i+1));
+        $(this).addClass('fade-in a' + parseInt(i+1));
       });
-    }
-    //
-    //
-    //
-    //     if (i === 0) {
-    //       $(this).addClass('fade-in one');
-    //     } else if (i === 1) {
-    //       $(this).addClass('fade-in two');
-    //     } else {
-    //       $(this).addClass('fade-in three');
-    //     }
-    //   });
-    // }
-
-    // active nav
-    if ($(window).scrollTop() >= top) {
-      $('nav').find('a').removeClass('active');
-      $('nav').find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
     }
   }
 
-
   $(window).scroll(function(){
-    sticky();
-    activeAndAnimations();
+    onScroll();
   });
 
   /* SMOOTH SCROLLING */
